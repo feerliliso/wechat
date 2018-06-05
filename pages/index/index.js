572ce5f55d6b353cf63fa54151b96c0332047f15
feerliliso <feerliliso@hotmail.com>
2018-06-05 15:52:59 +0800

    add image
    rt

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,41 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+Page({
+data:{
+nowtemp:'',
+nowweather:''
+},
+
+onLoad(){
+  const requestTask = wx.request({
+    url: 'https://test-miniprogram.com/api/weather/now', 
+    data: {
+     city:'上海市'
+    },
+    success: res=> {
+      console.log(res)
+      let result = res.data.result
+      console.log(result)
+      let temp = result.now.temp
+      console.log(temp)
+      let weather = result.now.weather
+      console.log(weather)
+      console.log(temp,weather)
+      this.setData({
+        nowtemp:temp,
+        nowweather:weatherMap[weather]
+      })
+    }
+  })
+
+  
+}
+
+})
